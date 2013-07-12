4bf1241e3221cc4a2f56361a585c58e57d60d716
Dan Berindei <dan@infinispan.org>
2013-07-12 14:25:46 +0300

    Support compressed logs not named infinispan.log.gz

--- 2failedTestLogs.sh
+++ 2failedTestLogs.sh
@@ -1,31 +1,28 @@
 #!/bin/sh
 
 DIR=`dirname $0`
-CAT=cat
 #echo dir=$DIR
 
 if [ -f "$1" ] ; then FILE=$1 ; shift ; fi
 if [ -n "$1" ] ; then FAILED_TESTS="$*" ; fi
 
 if [ ! -f "$FILE" ] ; then
-  FILE=infinispan.log
-  if [ ! -f "$FILE" ] ; then
-    ZFILE=infinispan.log.gz
-    CAT=zcat
-  fi
+  FILE=`ls -t infinispan.log* | head -1`
+fi
+
+CAT=cat
+if [ "${FILE##*.}" == "gz" ] ; then
+  CAT=zcat
 fi
 
 if [ ! -f "$FILE" ] ; then
-  echo "Could not open infinispan.log or infinispan.log.gz"
+  echo "Could not find infinispan.log or infinispan.log.gz"
 fi
 
 echo Processing $FILE
-export CAT FILE
-
 
 if [ -z "$FAILED_TESTS" ] ; then
-  FAILED_TESTS=`$CAT $FILE | egrep "UnitTestTestNGListener.*(failed|skipped)" | perl -ne '/Test .*\(.*\.(.*)\)/; print "$1\n";' | sort | uniq`
-  #FAILED_TESTS=`$CAT $FILE | egrep "(Test failed)|(skipped.\$)" | perl -ne '/.*\(.*\.(.*)\)$/; print "$1\n";' | sort | uniq`
+  FAILED_TESTS=`$CAT $FILE | perl -ne '/Test .*\(.*\.(.*)\) (failed|skipped)\./ && print "$1\n";' | sort -u`
 fi
 echo Failed/skipped tests: $FAILED_TESTS 
 
