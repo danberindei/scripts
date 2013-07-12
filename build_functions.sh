18e1a02d9d1887cb4d58f9870685db2e99db8cfb
Dan Berindei <dan@infinispan.org>
2013-07-12 15:30:13 +0300

    No longer build each branch in a separate directory Use the test goal instead of surefire:test

--- build_functions.sh
+++ build_functions.sh
@@ -13,13 +13,10 @@ function big_build_function() {
     # maven options
     #MAVEN_TUNING_OPTS="-server -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewRatio=3"
     MAVEN_TUNING_OPTS="-server -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewRatio=4"
-    MAVEN_MEMORY_OPTS="-Xmx900m -XX:MaxPermSize=300m -Xss500k -XX:+HeapDumpOnOutOfMemoryError"
+    MAVEN_MEMORY_OPTS="-Xmx500m -XX:MaxPermSize=200m -Xss500k -XX:+HeapDumpOnOutOfMemoryError"
     MAVEN_JGROUPS_OPTS="-Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=127.0.0.1"
     # for tests only
-    #MAVEN_DEBUG_OPTS="-ea"
-    #MAVEN_DEBUG_OPTS="-ea -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5006"
     MAVEN_DEBUG_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
-    MAVEN_LOG_OPTS=""
     MAVEN_LOG_OPTS="-Dlog4j.configuration=file:/home/dan/Work/infinispan/log4j-trace.xml"
 
     MAVEN_OPTS="$MAVEN_TUNING_OPTS $MAVEN_MEMORY_OPTS $MAVEN_JGROUPS_OPTS $EXTRA_BUILD_OPTS"
@@ -44,16 +41,18 @@ function big_build_function() {
     PROJECT=`basename \`pwd\``
     echo Running off branch $BRANCH
     UPSTREAM_BRANCH=`find_upstream_branch $BRANCH`
-    DEST=/tmp/privatebuild/$PROJECT/$UPSTREAM_BRANCH
+#    DEST=/tmp/privatebuild/$PROJECT/$UPSTREAM_BRANCH
+    DEST=/tmp/privatebuild/$PROJECT
 
     if [ -z "$UPSTREAM_BRANCH" ] ; then
-      exit 1
+      return 1
     fi
 
     if [ -n "$EXPLICIT_BRANCH" ] ; then
       print_and_run rm -r $DEST
       print_and_run git clone -slb "$BRANCH" . $DEST
     else
+      print_and_run rm -r $DEST
       print_and_run mkdir -p $DEST
       print_and_run rsync -a --delete --link-dest=. --exclude '.git' --exclude 'target' --exclude '*.log' . $DEST
     fi
@@ -74,31 +73,30 @@ function big_build_function() {
     echo Building modules $MODULE_ARGS
     EXTRA_ARGS="$*"
 
+    ERRCODE=0
     if [ $CLEAN_BUILD -eq 1 ] ; then
       print_and_run mvn -nsu -P-extra clean ${=MODULE_ARGS} ${=EXTRA_ARGS}
+      ERRCODE=$?
     fi
-    if [ $? -ne 0 ] ; then
-      exit 9
+    if [ $ERRCODE -ne 0 ] ; then
+      return $ERRCODE
     fi
 
     if [ $BUILD -eq 1 ] ; then
       print_and_run mvn -nsu -P-extra install -DskipTests -am ${=MODULE_ARGS} ${=EXTRA_ARGS}
+      ERRCODE=$?
     fi
-    if [ $? -ne 0 ] ; then
-      exit 9
+    if [ $ERRCODE -ne 0 ] ; then
+      return $ERRCODE
     fi
 
     if [ $TEST -eq 1 ] ; then
       if [ $DEBUG -ne 1 ] ; then
         MAVEN_DEBUG_OPTS=""
       fi
-      MAVEN_OPTS="$MAVEN_OPTS $MAVEN_DEBUG_OPTS $MAVEN_LOG_OPTS $EXTRA_TEST_OPTS" print_and_run mvn surefire:test ${=MODULE_ARGS} ${=EXTRA_ARGS}
+      MAVEN_OPTS="$MAVEN_OPTS $MAVEN_DEBUG_OPTS $MAVEN_LOG_OPTS $EXTRA_TEST_OPTS" print_and_run mvn test ${=MODULE_ARGS} ${=EXTRA_ARGS}
       ERRCODE=$?
-#      if [ $ERRCODE -ne 0 ] ; then
-#        print_and_run popd
-#        print_and_run ../failedTestLogs.sh $LOGS_DIR/infinispan.log
-#        return 9
-#      fi
+      return $ERRCODE
     fi
 }
 
