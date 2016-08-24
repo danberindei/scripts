6ef23af203cabcedc23d91b189c5b4c5a9656e01
Dan Berindei <dan@infinispan.org>
2016-08-24 15:21:21 +0300

    Stop changing symbolic links in javaX functions

--- oh-my-zsh/dev_functions.zsh
+++ oh-my-zsh/dev_functions.zsh
@@ -1,24 +1,24 @@
 function openjdk8 {
-  ln -sfT $OPEN_JDK8_HOME $JAVA_HOME
+  JAVA_HOME=$OPEN_JDK8_HOME
   echo JAVA_HOME=$(readlink $JAVA_HOME)
 }
 
 function openjdk7 {
-  ln -sfT $OPEN_JDK7_HOME $JAVA_HOME
+  JAVA_HOME=$OPEN_JDK7_HOME
   echo JAVA_HOME=$(readlink $JAVA_HOME)
 }
 
 function java6 {
-  ln -sfT $ORACLE_JDK8_HOME $JAVA_HOME
+  JAVA_HOME=$ORACLE_JDK6_HOME
   echo JAVA_HOME=$(readlink $JAVA_HOME)
 }
 
 function java7 {
-  ln -sfT $ORACLE_JDK7_HOME $JAVA_HOME
+  JAVA_HOME=$ORACLE_JDK7_HOME
   echo JAVA_HOME=$(readlink $JAVA_HOME)
 }
 
 function java8 {
-  ln -sfT $ORACLE_JDK8_HOME $JAVA_HOME
+  JAVA_HOME=$ORACLE_JDK8_HOME
   echo JAVA_HOME=$(readlink $JAVA_HOME)
 }
