28e282c93bc59d34a063aa9232c132dfe410bb9a
Dan Berindei <dan@infinispan.org>
2017-10-20 18:26:58 +0300

    Simplified test tracking, UseTransparentHugePages, HeapDumpOnOutOfMemoryError

--- build_functions.py
+++ build_functions.py
@@ -4,9 +4,9 @@ import re
 import shlex
 import signal
 
-from subprocess import *
 from os import chdir, environ, getcwd, path
 from pipes import quote
+from subprocess import *
 from sys import argv, exit, stderr, stdout
 
 debug = True
@@ -24,22 +24,14 @@ def test(cmd):
   return errcode == 0
 
 def filter_test_output(input):
-  regex2 = re.compile("(?:^|\))\[TestSuiteProgress\] Test (starting|succeeded|failed|skipped):")
-  stats = dict()
+  regex = re.compile("(?:^|(?<=\)))\[TestSuiteProgress\] (Tests succeeded: .*)$")
 
   line = input.readline()
   while line:
-    m = regex2.search(line)
+    m = regex.search(line)
     if m:
       # write the number of tests in the title
-      status = m.group(1)
-      stats[status] = stats.get(status, 0) + 1
-      started = stats.get("starting", 0)
-      succeeded = stats.get("succeeded", 0)
-      failed = stats.get("failed", 0)
-      skipped = stats.get("skipped", 0)
-      running = started - succeeded - failed
-      stderr.write("\033]0;Tests successful: {1}, failed: {2}, skipped: {3}, running: {0}\a".format(running, succeeded, failed, skipped))
+      stderr.write("\033]0;{0}\a".format(m.group(1)))
 
     stdout.write(line)
     line = input.readline()
@@ -53,29 +45,33 @@ def big_build_function(CLEAN_BUILD, BUILD, TEST):
     EXTRA_TEST_OPTS=environ.get("EXTRA_TEST_OPTS", "")
 
     # maven options
+    MAVEN_GENERIC_OPTS="-Dsurefire.skipAfterFailureCount=100"
     #MAVEN_TUNING_OPTS="-server -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseParallelOldGC -XX:NewRatio=3 -XX:+TieredCompilation"
-    MAVEN_TUNING_OPTS="-server -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+TieredCompilation"
+    MAVEN_TUNING_OPTS="-server -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:+TieredCompilation -XX:+UseTransparentHugePages"
     #MAVEN_TUNING_OPTS="-server -XX:-UseCompressedOops -XX:+UseG1GC -XX:MaxGCPauseMillis=500 -XX:-TieredCompilation"
     #MAVEN_TUNING_OPTS=""
     #MAVEN_TUNING_OPTS="-server -XX:+AggressiveOpts -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewRatio=4 -XX:-UseLargePages"
     #MAVEN_TUNING_OPTS="-server -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:NewRatio=4"
     #MAVEN_MEMORY_OPTS="-Xmx1000m -XX:MaxPermSize=320m -Xss1m -XX:+HeapDumpOnOutOfMemoryError"
-    MAVEN_MEMORY_OPTS="-Xmx1500m -Xss1m"
-    FORK_MEMORY_OPTS="-Xmx2000m -Xss1m"
+    MAVEN_MEMORY_OPTS="-Xmx1000m -Xss1m -XX:+HeapDumpOnOutOfMemoryError"
+    #MAVEN_MEMORY_OPTS="-Xmx1500m -Xss1m"
+    #FORK_MEMORY_OPTS="-Xmx2000m -Xss1m -XX:MaxPermSize=320m -XX:+HeapDumpOnOutOfMemoryError"
+    FORK_MEMORY_OPTS="-Xmx2000m -Xss1m -XX:+HeapDumpOnOutOfMemoryError"
     MAVEN_JGROUPS_OPTS="-Djava.net.preferIPv4Stack=true -Djgroups.bind_addr=127.0.0.1"
     # for tests only
-    MAVEN_DEBUG_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5006"
-    MAVEN_LOG_OPTS="-Dlog4j.configuration=file:/home/dan/Work/infinispan/log4j-trace.xml"
-    MAVEN_LOG_OPTS="-Dlog4j.configurationFile=file:///home/dan/Work/infinispan/log4j2-trace.xml -Dinfinispan.log.path=" + LOGS_DIR + " " + MAVEN_LOG_OPTS
-    #MAVEN_GC_LOG_OPTS="-XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDetails -Xloggc:/home/dan/Work/logs/gc.log"
-    MAVEN_GC_LOG_OPTS=""
+    #MAVEN_DEBUG_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=5006"
+    MAVEN_DEBUG_OPTS="-ea -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5006"
+    MAVEN_LOG_OPTS="-Dlog4j.configuration=file:/home/dan/Work/infinispan/log4j1-trace.xml"
+    MAVEN_LOG_OPTS="-Dlog4j.configurationFile=file:///home/dan/Work/infinispan/log4j2-trace.xml -Dinfinispan.log.path=" + LOGS_DIR + " -DAsyncLogger.ThreadNameStrategy=UNCACHED " + MAVEN_LOG_OPTS
+    MAVEN_GC_LOG_OPTS="-XX:+PrintGCTimeStamps -XX:+PrintGCApplicationStoppedTime -XX:+PrintGCDetails -Xloggc:/home/dan/Work/logs/gc.log"
+    #MAVEN_GC_LOG_OPTS=""
 
     #MAVEN_OPTS=" ".join((MAVEN_TUNING_OPTS, MAVEN_JGROUPS_OPTS, EXTRA_BUILD_OPTS))
 
-    if not environ.get("MAVEN_DEBUG"):
+    if not environ.get("DEBUG_TESTS"):
       MAVEN_DEBUG_OPTS=""
 
-    MAVEN_OPTS=" ".join((MAVEN_TUNING_OPTS, MAVEN_MEMORY_OPTS, MAVEN_JGROUPS_OPTS, EXTRA_BUILD_OPTS))
+    MAVEN_OPTS=" ".join((MAVEN_GENERIC_OPTS, MAVEN_TUNING_OPTS, MAVEN_MEMORY_OPTS, MAVEN_JGROUPS_OPTS, EXTRA_BUILD_OPTS))
     #MAVEN_FORK_OPTS=" ".join((MAVEN_OPTS, MAVEN_DEBUG_OPTS, EXTRA_TEST_OPTS))
     MAVEN_FORK_OPTS=" ".join((MAVEN_OPTS, MAVEN_LOG_OPTS, MAVEN_GC_LOG_OPTS, MAVEN_DEBUG_OPTS, EXTRA_TEST_OPTS))
 
@@ -85,7 +81,8 @@ def big_build_function(CLEAN_BUILD, BUILD, TEST):
       EXPLICIT_BRANCH=True
       remaining_args=argv[2:]
     else:
-      BRANCH=run("git symbolic-ref --short HEAD").strip()
+      #BRANCH=run("git symbolic-ref --short HEAD").strip()
+      BRANCH=run("git name-rev --name-only HEAD").strip()
       EXPLICIT_BRANCH=False
       remaining_args=argv[1:]
 
@@ -94,12 +91,8 @@ def big_build_function(CLEAN_BUILD, BUILD, TEST):
 
     PROJECT=path.basename(getcwd())
     print "Running off branch %s" % BRANCH
-    UPSTREAM_BRANCH=run("find_upstream_branch %s" % quote(BRANCH))
-    #DEST="/tmp/privatebuild/%s" % quote(PROJECT)
-    DEST="../tmpbuild/%s" % quote(PROJECT)
 
-    if not UPSTREAM_BRANCH:
-      exit(1)
+    DEST="../tmpbuild/%s" % quote(PROJECT)
 
     if EXPLICIT_BRANCH:
       if CLEAN_BUILD == 1:
@@ -109,8 +102,11 @@ def big_build_function(CLEAN_BUILD, BUILD, TEST):
         stderr.write("Cannot test a custom branch without building and cleaning the directory\n")
         exit(2)
     else:
+      rsync_delete = ""
+      if CLEAN_BUILD == 1:
+        rsync_delete = "--delete"
       print_and_run("mkdir -p %s" % quote(DEST))
-      print_and_run("rsync -a --delete --link-dest=. --exclude '.git' --exclude 'target' --exclude '*.log' . %s" % quote(DEST))
+      print_and_run("rsync -a %s --link-dest=. --exclude '.git' --exclude 'target' --exclude '*.log' . %s" % (rsync_delete, quote(DEST)))
 
     chdir(DEST)
 
@@ -129,25 +125,25 @@ def big_build_function(CLEAN_BUILD, BUILD, TEST):
 
     ERRCODE=0
     if CLEAN_BUILD == 1:
-      #print_and_run mvn -nsu clean -am ${=MODULE_ARGS}
-      print_and_run("mvn -nsu clean %s" % EXTRA_ARGS)
+      print_and_run("mvn -nsu clean -s maven-settings.xml %s" % EXTRA_ARGS)
 
     if BUILD == 1:
-      #print_and_run mvn -nsu -P-extras $CLEAN install -DskipTests -am ${=MODULE_ARGS} ${=EXTRA_ARGS}
-      print_and_run("mvn -nsu install -DskipTests -am %s %s" % (MODULE_ARGS, EXTRA_ARGS))
+      print_and_run("mvn -nsu install -s maven-settings.xml -DskipTests -am %s %s" % (MODULE_ARGS, EXTRA_ARGS))
 
     if TEST == 1:
       print "MAVEN_FORK_OPTS=%s" % MAVEN_FORK_OPTS
       environ["MAVEN_FORK_OPTS"] = MAVEN_FORK_OPTS
       #cmd = "mvn -e -o surefire:test failsafe:integration-test failsafe:verify -Ptest-CI %s %s %s" %(MODULE_ARGS, MAVEN_LOG_OPTS, EXTRA_ARGS)
-      cmd = "mvn -e -o verify -Ptest-CI %s %s %s" %(MODULE_ARGS, MAVEN_LOG_OPTS, EXTRA_ARGS)
+      #cmd = "mvn -o verify -Ptest-CI %s %s %s" %(MODULE_ARGS, MAVEN_LOG_OPTS, EXTRA_ARGS)
+      cmd = "mvn -o verify -s maven-settings.xml %s %s %s" %(MODULE_ARGS, MAVEN_LOG_OPTS, EXTRA_ARGS)
       pipe = Popen(shlex.split(cmd), stdout = PIPE, stderr = STDOUT)
 
       try:
         filter_test_output(pipe.stdout)
         returncode = pipe.wait()
-      except:
+      except Exception as e:
         pipe.terminate()
+        raise e
 
       if returncode != 0:
         raise CalledProcessError(returncode, cmd)
