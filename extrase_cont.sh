bc5b72b4599433e969551c11cc749e5349ef3598
Dan Berindei <dan@demandlane.com>
2024-09-24 15:00:14 +0300

    Relative paths

--- extrase_cont.sh
+++ extrase_cont.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(dirname $0)
+
 #Alpha
 for f in ?????????????.pdf ????????-????????*.pdf ; do
-  NEW_NAME="$(pdftotext "$f" - | perl ~/.local/bin/alpha_statement_name.pl)"
-  if [ ${#NEW_NAME} -gt 5 ] ; then
+  NEW_NAME="$(pdftotext "$f" - | perl $SCRIPT_DIR/alpha_statement_name.pl)"
+  if [ ${#NEW_NAME} -gt 10 ] ; then
     if [ ! -e "${NEW_NAME}.pdf" ] ; then
       echo mv "$f" "${NEW_NAME}.pdf"
       mv "$f" "${NEW_NAME}.pdf"
@@ -13,8 +15,8 @@ done
 
 #BT
 for f in *RO*BTRL*.pdf statement*.pdf; do
-  NEW_NAME="$(pdftotext "$f" - | perl ~/.local/bin/bt_statement_name.pl)"
-  if [ ${#NEW_NAME} -gt 5 ] ; then
+  NEW_NAME="$(pdftotext "$f" - | perl $SCRIPT_DIR/bt_statement_name.pl)"
+  if [ ${#NEW_NAME} -gt 10 ] ; then
     if [ ! -e "${NEW_NAME}.pdf" ] ; then
       echo mv "$f" "${NEW_NAME}.pdf"
       mv "$f" "${NEW_NAME}.pdf"
